c28ca3ba1a3dacbcafaab62cd186e9ed01eb0303
RidwanC134 <ridwanhh10@gmail.com>
2024-10-12 01:58:17 +0700

    Membuat Kelas Collections

---
+++ app/src/main/java/com/example/meet_1/Collections.kt
@@ -0,0 +1,7 @@
+package com.example.meet_1
+
+// List adalah Kumpulan data yang disusun secara berurutan dari
+// List bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
+// List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// List Read-Only menggunakan listOf
+// List Mutable menggunakan mutableListOf
