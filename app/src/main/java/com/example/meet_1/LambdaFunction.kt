1b7bba5856f1962bbb055b9022a2d3bbc28b0f5e
RidwanC134 <ridwanhh10@gmail.com>
2024-10-12 03:22:01 +0700

    Memanggil Fungsi uupercaseString dengan "hello" dan mencetaknya menjadi huruf kapital

--- app/src/main/java/com/example/meet_1/LambdaFunction.kt
+++ app/src/main/java/com/example/meet_1/LambdaFunction.kt
@@ -9,3 +9,10 @@ package com.example.meet_1
 fun uppercaseString(string: String): String {
     return string.uppercase()
 }
+
+//Dapat ditulis dalam ekspresi lambda sbb :
+fun main() {
+    uppercaseString("hello")
+    println({ string: String -> string.uppercase() }("hello"))
+// HELLO
+}
