672e42b7edb6544ea3c83fea6b79691dc07a245e
RidwanC134 <ridwanhh10@gmail.com>
2024-10-12 01:55:55 +0700

    Membuat Kelas TipeData

---
+++ app/src/main/java/com/example/meet_1/TipeData.kt
@@ -0,0 +1,36 @@
+package com.example.meet_1
+
+fun main() {
+
+    // Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    // Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    // Float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    // Double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    // Char
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+    // Boolean
+    var booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    // String
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin!"
+    println("String: $stringValue")
+
+}
